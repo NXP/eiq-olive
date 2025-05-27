6d47b1db585ab78864b1cb10cc0945716c55f612
Michaela Sezimova <michaela.sezimova@nxp.com>
2025-05-27 17:05:36 +0200

    [EITO-223] Added onnx2quant pass with tests.

---
+++ olive/passes/onnx/nxp_onnx2quant.py
@@ -0,0 +1,124 @@
+#
+# Copyright 2025 NXP
+#
+# License: LA_OPT_NXP_Software_License
+# See the LICENSE_LA_OPT_NXP_Software_License for more details.
+#
+
+from olive.passes.olive_pass import Pass
+from olive.passes.pass_config import BasePassConfig, PassConfigParam
+from typing import Dict, Type
+from olive.hardware import AcceleratorSpec
+from olive.model import ONNXModelHandler
+from pathlib import Path
+import onnx
+import logging
+
+from onnx2tflite.src.logger import conversion_log, MessageImportance
+from onnx2quant.__main__ import _parse_calibration_dataset_mapping
+from onnx2quant.__main__ import NpyCalibrationDataReader
+from onnx2quant.quantization_config import QuantizationConfig
+from onnx2quant.qdq_quantization import QDQQuantizer
+import onnx2tflite.src.converter.convert as convert
+
+logger = logging.getLogger(__name__)
+
+class ONNX2Quant(Pass):
+
+    @classmethod
+    def _default_config(cls, accelerator_spec: AcceleratorSpec) -> Dict[str, PassConfigParam]:
+        return {
+        "per_channel": PassConfigParam(
+            type_=bool,
+            default_value=False,
+            description="Quantize some weight tensors per-channel instead of per-tensor. This should result in a "
+                        "higher accuracy."
+        ),
+        "allow_opset_10_and_lower": PassConfigParam(
+            type_=bool,
+            default_value=False,
+            description="Allow quantization of models with opset version 10 and lower. Quantization of such models"
+                        "can produce invalid models because opset is forcefully updated to version 11. This applies "
+                        "especially to models with operators: Clip, Dropout, BatchNormalization and Split."
+        ),
+        "calibration_dataset_mapping": PassConfigParam(
+            type_=list[str],
+            required=True,
+            description="Mapping between model input and calibration dataset directory with *.npy files. Value must "
+                        "be in format '<input_name>;<path_to_dir>', for example 'input_1;data_3_224/'. Argument "
+                        "can be used multiple times to specify multiple inputs for the model. In case model"
+                        "has semicolon in input tensor's name, it has to be renamed."
+        ),
+        "symbolic_dimension_into_static": PassConfigParam(
+            type_=list[str],
+            default_value=[],
+            description="Change symbolic dimension in model to static (fixed) value. Provided mapping must "
+                        "follow this format '<dim_name>:<dim_size>', for example 'batch:1'."
+        ),
+        "set_input_shape": PassConfigParam(
+            type_=list[str],
+            default_value=[],
+            description="Override model input shape. Provided mapping must follow format '<dim_name>:(<dim_0>,"
+                        "<dim_1>,...)', for example 'input_1:(1,3,224,224)'."
+        )
+    }
+
+    def _log_quantization_logs(self):
+        def _get_logging_fn(importance):
+            severity_map = {
+                MessageImportance.DEBUG: logger.debug,
+                MessageImportance.INFO: logger.info,
+                MessageImportance.WARNING: logger.warning
+            }
+            return severity_map.get(importance, logger.error)
+
+        def _parse_log(log_category, log: dict):
+            # Log dictionary data:
+            # data = {
+            #     "message": message,
+            #     "logging_context_hierarchy": list(self._current_logging_context),
+            #     "importance": importance.value,
+            #     "message_id": self._log_count,
+            # }
+            return f'[ONNX2Quant:{log_category}] {log["message"]}'
+
+        for log_category, logs in conversion_log.get_logs().items():
+            for log in logs:
+                fn = _get_logging_fn(MessageImportance(log["importance"]))
+                fn(_parse_log(log_category, log))
+
+    def _run_for_config(
+        self,
+        model: ONNXModelHandler,
+        config: Type[BasePassConfig],
+        output_model_dir: str,
+    ) -> ONNXModelHandler:
+        if not isinstance(model, ONNXModelHandler):
+            raise NotImplementedError(f"Unsupported model handler type: {type(model)}")
+
+        config = dict(config)
+        try:
+            if "symbolic_dimension_into_static" in config:
+                    config["symbolic_dimensions_mapping"] = convert.parse_symbolic_dimensions_mapping(
+                        config["symbolic_dimension_into_static"]
+                    )
+
+            if "set_input_shape" in config:
+                config["input_shapes_mapping"] = convert.parse_input_shape_mapping(config["set_input_shape"])
+
+
+            calibration_dataset_mapping = _parse_calibration_dataset_mapping(config["calibration_dataset_mapping"])
+            calibration_data_reader = NpyCalibrationDataReader(calibration_dataset_mapping)
+            quantization_config = QuantizationConfig(calibration_data_reader, config)
+            onnx_model = onnx.load(model.model_path)
+            quantized_model = QDQQuantizer().quantize_model(onnx_model, quantization_config=quantization_config)
+        finally:
+            self._log_quantization_logs()
+
+        output_model_dir = Path(output_model_dir)
+        output_model_dir.mkdir(parents=True, exist_ok=True)
+        output_model_path = output_model_dir / "model_quantized.onnx"
+
+        onnx.save_model(quantized_model, output_model_path)
+
+        return ONNXModelHandler(output_model_path)
---
+++ test/nxp/passes/onnx/test_onnx2quant.py
@@ -0,0 +1,144 @@
+#
+# Copyright 2025 NXP
+#
+# License: LA_OPT_NXP_Software_License
+# See the LICENSE_LA_OPT_NXP_Software_License for more details.
+#
+
+import pytest
+import os
+import numpy as np
+import shutil
+from pydantic.v1 import ValidationError
+from olive.passes.olive_pass import create_pass_from_dict
+from olive.passes.onnx.nxp_onnx2quant import ONNX2Quant
+from test.unit_test.utils import get_onnx_model_config
+from pathlib import Path
+
+class randomCalibrationDataset:
+
+    def __init__(self, shape, path="tmp_calibration_dataset", np_type=np.float32, items_count=5):
+        self.path = path
+        self.shape = shape
+        self.np_type = np_type
+        self.items_count = items_count
+
+    def __enter__(self):
+
+        if os.path.exists(self.path):
+            raise Exception(f"Directory with name '{self.path}' already exists!")
+        Path.mkdir(self.path)
+
+        for x in range(self.items_count):
+            input_vector = np.random.random(self.shape).reshape(self.shape).astype(self.np_type)
+            np.save(os.path.join(self.path, f"{x}.npy"), input_vector)
+
+        return self.path
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        shutil.rmtree(self.path)
+
+def test_onnx2quant_pass_no_config():
+    """Test that pass without required parameters throws an error."""
+    with pytest.raises(ValidationError):
+        create_pass_from_dict(ONNX2Quant, {}, disable_search=True)
+
+def test_onnx2quant_with_minimum_arguments(tmp_path):
+    """Test the pass with only required argument."""
+    input_model = get_onnx_model_config().create_model()
+    with open(input_model.model_path, "rb") as f:
+        model = f.read()
+
+    with open("input_model.onnx", "wb") as f:
+        f.write(model)
+    config = {
+            "calibration_dataset_mapping": ["input;tmp_calibration_dataset"],
+            "allow_opset_10_and_lower": True
+        }
+    p = create_pass_from_dict(ONNX2Quant, config, disable_search=True)
+    with randomCalibrationDataset((1,1)):
+        output_folder = str(tmp_path)
+        onnx_model = p.run(input_model, output_folder)
+
+    assert Path(onnx_model.model_path).exists()
+
+def test_onnx2quant_all_parameters_defined(tmp_path):
+    """Test the pass with all parameters defined."""
+    input_model = get_onnx_model_config().create_model()
+    with open(input_model.model_path, "rb") as f:
+        model = f.read()
+
+    with open("input_model.onnx", "wb") as f:
+        f.write(model)
+    config = {
+            "calibration_dataset_mapping": ["input;tmp_calibration_dataset"],
+            "allow_opset_10_and_lower": True,
+            "per_channel": True,
+            "symbolic_dimension_into_static": ["batch:1"],
+            "set_input_shape":["input:(1,1)"]
+        }
+    p = create_pass_from_dict(ONNX2Quant, config, disable_search=True)
+    with randomCalibrationDataset((1,1)):
+        output_folder = str(tmp_path)
+        onnx_model = p.run(input_model, output_folder)
+
+    assert Path(onnx_model.model_path).exists()
+
+def test_onnx2quant_invalid_calibration_dataset_mapping(tmp_path):
+    """Test that the pass with wrong calibration_dataset_mapping configuration raises an error."""
+    input_model = get_onnx_model_config().create_model()
+    with open(input_model.model_path, "rb") as f:
+        model = f.read()
+
+    with open("input_model.onnx", "wb") as f:
+        f.write(model)
+    config = {
+            "calibration_dataset_mapping": ["input:tmp_calibration_dataset"],
+            "allow_opset_10_and_lower": True,
+        }
+    p = create_pass_from_dict(ONNX2Quant, config, disable_search=True)
+
+    with randomCalibrationDataset((1,1)):
+        output_folder = str(tmp_path)
+        with pytest.raises(Exception, match=r"in invalid format. Must be '<input_name>;<path_to_dataset>'"):
+            p.run(input_model, output_folder)
+
+def test_onnx2quant_invalid_symbolic_dim_into_static(tmp_path):
+    """Test that the pass with wrong symbolic_dim_into_static configuration raises an error."""
+    input_model = get_onnx_model_config().create_model()
+    with open(input_model.model_path, "rb") as f:
+        model = f.read()
+
+    with open("input_model.onnx", "wb") as f:
+        f.write(model)
+    config = {
+            "calibration_dataset_mapping": ["input;tmp_calibration_dataset"],
+            "allow_opset_10_and_lower": True,
+            "symbolic_dimension_into_static": ["batch:e"]
+        }
+    p = create_pass_from_dict(ONNX2Quant, config, disable_search=True)
+
+    with randomCalibrationDataset((1,1)):
+        output_folder = str(tmp_path)
+        with pytest.raises(Exception, match=r"in invalid format. Must be '<dim_name>:<dimension_size>'"):
+            p.run(input_model, output_folder)
+
+def test_onnx2quant_invalid_input_shape(tmp_path):
+    """Test that the pass raises an exception when input shape is invalid."""
+    input_model = get_onnx_model_config().create_model()
+    with open(input_model.model_path, "rb") as f:
+        model = f.read()
+
+    with open("input_model.onnx", "wb") as f:
+        f.write(model)
+    config = {
+            "calibration_dataset_mapping": ["input;tmp_calibration_dataset"],
+            "allow_opset_10_and_lower": True,
+            "set_input_shape": ["input:(a,b,c,d)"]
+        }
+    p = create_pass_from_dict(ONNX2Quant, config, disable_search=True)
+
+    with randomCalibrationDataset((1,1)):
+        output_folder = str(tmp_path)
+        with pytest.raises(Exception, match=r"in invalid format. Must be <dim_name>:\(<dim_0>,<dim_1>,...\)"):
+            p.run(input_model, output_folder)
